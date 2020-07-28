ecb10226d401c838592bce9fca7e61605b1ed0c7
xelinel32 <artemsedlyar99@gmail.com>
2020-07-28 16:11:15 +0300

    clean and setting project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/style.scss'
+import '../node_modules/reset-css/sass/_reset.scss'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount('#app')
